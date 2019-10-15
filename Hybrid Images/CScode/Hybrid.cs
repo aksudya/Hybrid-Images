34c5ae1d5c3e131ccaad646290d7ff7855aa4836
aksudya <467498869@qq.com>
2019-10-16 00:00:56 +0800

    Initial commit

---
+++ Hybrid Images/CScode/Hybrid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using OpenCvSharp;
+using Size = OpenCvSharp.Size;
+
+namespace Hybrid_Images.CScode
+{
+    class Hybrid
+    {
+        public static Hybrid instance;
+        public Mat img1;
+        public Mat img2;
+        public Mat img_target;
+        public BitmapImage img_tar;
+
+        public double frequency;
+        public int ksize;
+
+        public Hybrid()
+        {
+            frequency = 0.1;
+            ksize = 1;
+            instance = this;
+        }
+
+        public void caculate()
+        {
+            if (img1 == null || img2 == null)
+            {
+                return;
+            }
+
+            Mat img11=img1.Clone();
+
+            Mat img22 = img2.Clone();
+           
+            Cv2.GaussianBlur(img1, img11, new Size(ksize,ksize), frequency);
+            Cv2.GaussianBlur(img2, img22, new Size(ksize,ksize), frequency);
+            img22 = img2 - img22;
+            img_target = img22 + img11;
+            img_tar= MatToBitmapImage(img_target);
+        }
+
+        public Bitmap MatToBitmap(Mat image)
+        {
+            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+        }
+
+        public BitmapImage MatToBitmapImage(Mat image)
+        {
+            Bitmap bitmap = MatToBitmap(image);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                stream.Position = 0;
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+
+    }
+}
---
+++ Hybrid Images/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hybrid_Images.CScode;
+using OpenCvSharp;
+using Window = System.Windows.Window;
+
+namespace Hybrid_Images
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Hybrid hybrid=new Hybrid();
+            InitializeComponent();
+        }
+
+        private void img1_click(object sender, RoutedEventArgs e)
+        {
+            string storepath;
+            storepath = chooseimage(sender, e);
+       
+            if (storepath != "")
+            {
+               Hybrid.instance.img1 = new Mat(@storepath, ImreadModes.AnyColor | ImreadModes.AnyDepth);
+            }
+
+            img1.Source = Hybrid.instance.MatToBitmapImage(Hybrid.instance.img1);
+            if (Hybrid.instance.img1 != null)
+            {
+                Hybrid.instance.caculate();
+                imgDst.Source = Hybrid.instance.img_tar;
+            }
+
+        }
+
+        private void img2_click(object sender, RoutedEventArgs e)
+        {
+            string storepath;
+            storepath = chooseimage(sender, e);
+            if (storepath != "")
+            {
+                Hybrid.instance.img2= new Mat(@storepath, ImreadModes.AnyColor | ImreadModes.AnyDepth);
+            }
+
+            img2.Source = Hybrid.instance.MatToBitmapImage(Hybrid.instance.img2);
+            if (Hybrid.instance.img2 != null)
+            {
+                Hybrid.instance.caculate();
+                imgDst.Source= Hybrid.instance.img_tar;
+            }
+        }
+
+        private void switch_click(object sender, RoutedEventArgs e)
+        {
+            Mat temp = Hybrid.instance.img2;
+            Hybrid.instance.img2 = Hybrid.instance.img1;
+            Hybrid.instance.img1 = temp;
+
+            img1.Source = Hybrid.instance.MatToBitmapImage(Hybrid.instance.img1);
+            img2.Source = Hybrid.instance.MatToBitmapImage(Hybrid.instance.img2);
+
+            Hybrid.instance.caculate();
+            imgDst.Source = Hybrid.instance.img_tar;
+        }
+
+        private string chooseimage(object sender, RoutedEventArgs e)
+        {
+            string storepath = "";
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "image file|*.jpg;*.png;*.jpeg;*.bmp"
+            };
+            var result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                storepath = openFileDialog.FileName;
+            }
+            return storepath;
+        }
+
+        private void fre_change(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Hybrid.instance.frequency = frequent.Value;
+            if (Hybrid.instance.img1 != null&& Hybrid.instance.img2 != null)
+            {
+                Hybrid.instance.caculate();
+                imgDst.Source = Hybrid.instance.img_tar;
+            }
+            frequent_lable.Content = Hybrid.instance.frequency.ToString();
+        }
+
+        private void ksize_change(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Hybrid.instance.ksize = (int)ksize.Value*2+1;
+            if (Hybrid.instance.img1 != null && Hybrid.instance.img2 != null)
+            {
+                Hybrid.instance.caculate();
+                imgDst.Source = Hybrid.instance.img_tar;
+            }
+
+            ksize_lable.Content = Hybrid.instance.ksize.ToString();
+        }
+    }
+}
